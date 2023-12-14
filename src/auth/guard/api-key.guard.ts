f19a0673b842484de5fbd5f5227c03fa0d5492ec
woo3145 <lcwoo3145@gmail.com>
2023-12-14 17:39:09 +0900

    ApiKey Guard 수정

--- src/auth/guard/api-key.guard.ts
+++ src/auth/guard/api-key.guard.ts
@@ -2,4 +2,4 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class ApiKeyGuard extends AuthGuard('api-key') {}
+export class ApiKeyGuard extends AuthGuard('apiKey') {}
